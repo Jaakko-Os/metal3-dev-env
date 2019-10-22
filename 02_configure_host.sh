036813d9681d10eea12b6a86ef88561a598dbbf6
Nam Xuan Nguyen <nam.xuan.nguyen@est.tech>
2019-10-22 21:50:38 +0200

    Fix

--- 02_configure_host.sh
+++ 02_configure_host.sh
@@ -57,14 +57,18 @@ else
       if [ ! -e /etc/sysconfig/network-scripts/ifcfg-provisioning ] ; then
           echo -e "DEVICE=provisioning\nTYPE=Bridge\nONBOOT=yes\nNM_CONTROLLED=no\nBOOTPROTO=static\nIPADDR=172.22.0.1\nNETMASK=255.255.255.0" | sudo dd of=/etc/sysconfig/network-scripts/ifcfg-provisioning
       fi
-      sudo ip link set provisioning down || true
-      sudo ip link set provisioning up || true
+      # sudo ip link set provisioning down || true
+      # sudo ip link set provisioning up || true
+      sudo ifdown provisioning  || true
+      sudo ifup provisioning  || true
 
       # Need to pass the provision interface for bare metal
       if [ "$PRO_IF" ]; then
           echo -e "DEVICE=$PRO_IF\nTYPE=Ethernet\nONBOOT=yes\nNM_CONTROLLED=no\nBRIDGE=provisioning" | sudo dd of="/etc/sysconfig/network-scripts/ifcfg-$PRO_IF"
-          sudo ip link set "$PRO_IF" down || true
-          sudo ip link set "$PRO_IF" up || true
+          # sudo ip link set "$PRO_IF" down || true
+          # sudo ip link set "$PRO_IF" up || true
+          sudo ifdown "$PRO_IF" || true
+          sudo ifup "$PRO_IF" || true
       fi
   fi
 
@@ -73,8 +77,8 @@ else
       if [ ! -e /etc/sysconfig/network-scripts/ifcfg-baremetal ] ; then
           echo -e "DEVICE=baremetal\nTYPE=Bridge\nONBOOT=yes\nNM_CONTROLLED=no" | sudo dd of=/etc/sysconfig/network-scripts/ifcfg-baremetal
       fi
-      sudo ip link set baremetal down || true
-      sudo ip link set baremetal up || true
+      sudo ifdown baremetal  || true
+      sudo ifup baremetal  || true
 
       # Add the internal interface to it if requests, this may also be the interface providing
       # external access so we need to make sure we maintain dhcp config if its available
@@ -94,7 +98,8 @@ else
       sudo virsh net-destroy baremetal
       sudo virsh net-start baremetal
       if [ "$INT_IF" ]; then #Need to bring UP the NIC after destroying the libvirt network
-          sudo ip link set "$INT_IF" up || true
+          # sudo ip link set "$INT_IF" up || true
+          sudo ifup "$INT_IF" || true
       fi
   fi
 fi
